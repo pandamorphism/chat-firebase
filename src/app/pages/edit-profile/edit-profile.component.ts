eff5c0e7204e0917599106419f23d5140df0c7c0
Oleg Dmytriienko <plammtech@gmail.com>
2019-01-20 23:50:13 +0200

    fp-ts! no more null pointers

--- src/app/pages/edit-profile/edit-profile.component.ts
+++ src/app/pages/edit-profile/edit-profile.component.ts
@@ -1,30 +1,47 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {take, tap} from 'rxjs/operators';
 import {AuthService} from '../../shared/services/auth.service';
+import {UserService} from '../../shared/services/user.service';
+import {ActivatedRoute} from '@angular/router';
+import {tag} from 'rxjs-spy/operators';
+import {profileIdLens} from '../../shared/misc/pure.utils';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
   styleUrls: ['./edit-profile.component.scss']
 })
-export class EditProfileComponent implements OnInit {
+export class EditProfileComponent implements OnInit, OnDestroy {
   profileForm: FormGroup;
 
   constructor(private auth: AuthService,
+              private route: ActivatedRoute,
+              private userService: UserService,
               private fb: FormBuilder) {
   }
 
   ngOnInit() {
+    profileIdLens.asOptional().getOption(this.route.snapshot.params as { profileId: string }).map(this.bindUserToForm.bind(this));
     this.profileForm = this.fb.group({
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
       quote: [''],
     });
-    this.auth.currentUser$.pipe(
+  }
+
+  bindUserToForm(userId: string) {
+    this.userService.getById(userId).pipe(
       tap(({firstName, lastName, quote}) => this.profileForm.setValue({firstName, lastName, quote})),
-      take(1)
+      take(1),
+      tag('profileEditStream'),
     ).subscribe();
   }
 
+  submit() {
+    console.log('submitting...');
+  }
+
+  ngOnDestroy(): void {
+  }
 }
--- src/app/shared/misc/pure.utils.ts
+++ src/app/shared/misc/pure.utils.ts
@@ -1,11 +1,14 @@
 import {Observable} from 'rxjs';
 import {filter} from 'rxjs/operators';
+import {Lens} from 'monocle-ts';
 
 export const IS_TRUE: (x: boolean) => boolean = x => x === true;
 export const NOT: (x: boolean) => boolean = x => !x;
 export const IS_FALSE: (x: boolean) => boolean = x => NOT(IS_TRUE(x));
 export const NOT_NULL: (x: any) => boolean = x => x !== undefined && x !== null;
 export const NOT_EMPTY: (x: any[] | string) => boolean = x => NOT_NULL(x) && x.length > 0;
+
+export const profileIdLens = Lens.fromProp<{ profileId: string }>()('profileId');
 
 export const partition: <T>(predicate: (val: T) => boolean) => (source: Observable<T>) => Observable<T>[] =
   predicate => source =>
