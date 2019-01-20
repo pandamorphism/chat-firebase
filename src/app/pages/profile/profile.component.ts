e11cdab7f17190bd3b48d563649316f9c75b76b3
Oleg Dmytriienko <plammtech@gmail.com>
2019-01-20 21:50:44 +0200

    fixed streams in user profile

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {filter, map, switchMap, tap} from 'rxjs/operators';
+import {FormBuilder} from '@angular/forms';
+import {filter, map, shareReplay, switchMap} from 'rxjs/operators';
 import {ActivatedRoute, Router} from '@angular/router';
 import {tag} from 'rxjs-spy/operators';
 import {NOT_EMPTY} from '../../shared/misc/pure.utils';
@@ -8,6 +8,7 @@ import {UserService} from '../../shared/services/user.service';
 import {combineLatest, Observable} from 'rxjs';
 import {User} from '../../shared/model/user';
 import {AuthService} from '../../shared/services/auth.service';
+import {untilDestroyed} from 'ngx-take-until-destroy';
 
 @Component({
   selector: 'app-profile',
@@ -15,7 +16,6 @@ import {AuthService} from '../../shared/services/auth.service';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit, OnDestroy {
-  profileForm: FormGroup;
   user$: Observable<User>;
   private isCurrentUserProfile$: Observable<boolean>;
 
@@ -24,18 +24,19 @@ export class ProfileComponent implements OnInit, OnDestroy {
               private route: ActivatedRoute,
               private router: Router,
               private userService: UserService) {
-    this.user$ = this.route.paramMap.pipe(
-      map(params => params.get('profileId')),
-      filter(NOT_EMPTY),
-      tap(profileId => console.log('profileId: %O', profileId)),
-      switchMap((id: string) => this.userService.getById(id)),
-      tag('profileGetterStream')
-    );
-    this.isCurrentUserProfile$ = combineLatest(this.user$, this.authService.currentUser$)
-      .pipe(map(([profile, currentUser]) => profile.uid === currentUser.uid));
   }
 
   ngOnInit() {
+    this.user$ = this.route.paramMap.pipe(
+      map(params => params.get('profileId')),
+      filter(NOT_EMPTY),
+      switchMap((id: string) => this.userService.getById(id)),
+      shareReplay(1),
+      tag('profileGetterStream'),
+      untilDestroyed(this)
+    );
+    this.isCurrentUserProfile$ = combineLatest(this.user$, this.authService.currentUser$)
+      .pipe(map(([profile, currentUser]) => profile && currentUser && profile.uid === currentUser.uid), untilDestroyed(this));
   }
 
   ngOnDestroy(): void {
